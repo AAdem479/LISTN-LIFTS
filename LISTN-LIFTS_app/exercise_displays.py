826dc53ba8af4e61a883e2d1f40ec3a88c45b02f
AAAABlack <ademblack2077@gmailcom>
2024-11-30 14:59:11 +0100

    useful comments, warmup

--- LISTN-LIFTS_app/exercise_displays.py
+++ LISTN-LIFTS_app/exercise_displays.py
@@ -3,10 +3,10 @@ from data import exercises
 
 
 def display_exercises(status):
-    if len(exercises) == 0:
+    if len(exercises) == 0: # checking if dictionary has keys
         print("No exercises found.")
-    match status:
-        case "completed":
+    match status: # match casing instead of if else statements
+        case "completed": 
             print("Your finished exercises are: ")
         case "not completed":
             print("You still have to do: ")
@@ -14,7 +14,7 @@ def display_exercises(status):
             print("Your exercises for the day are: ")
     for i, (exercise_details, current_status) in enumerate(exercises.items(), 1):
         exercise_name, sets, reps = exercise_details
-        if status is None:
+        if status is None: # outputs ALL exercises regardless of status
             print(f"{i}: Workout: {exercise_name}, Sets: {sets}, Reps: {reps}, Status: {current_status['status']}")
-        elif current_status['status'] == status:
+        elif current_status['status'] == status: # outputs exercises based on status = ("completed" or "not completed")
             print(f"{i}: Workout: {exercise_name}, Sets: {sets}, Reps: {reps}, Status: {current_status['status']}")
